2c58dd651d3c12f115677ea1f4039f1d5d536771
Abd Elrhman Mohamed Shata <abdoelrhmanshata@yahoo.com>
2023-11-26 21:36:39 +0200

    Html_Css_Js Project

--- Html_Css_Js Project/Code/codeHome.js
+++ Html_Css_Js Project/Code/codeHome.js
@@ -1,5 +1,6 @@
 //////////////////////////////////////////////////
 // Database 
+var cartProduct = [];
 // Slider Images
 var images = [
     "../images/slider/sliderImage1.jpg",
@@ -16,7 +17,7 @@ var phoneProducts = [
         title: "iPhone 15 Pro Max",
         description: "iPhone 15 Pro Max (256 GB) - Black Titanium Dual Sim",
         rating: 4,
-        price: "75,555 EGP",
+        price: 75555
     },
     {
         id: "2",
@@ -24,7 +25,7 @@ var phoneProducts = [
         title: "iPhone 14 Pro",
         description: "Apple iPhone 14 with FaceTime - 128GB, 4GB RAM, 4G LTE, Black, Single SIM & E-SIM",
         rating: 3,
-        price: "39,990 EGP",
+        price: 39990
     },
     {
         id: "3",
@@ -32,7 +33,7 @@ var phoneProducts = [
         title: "Xiaomi Redmi 10",
         description: "Xiaomi Redmi 10 Dual SIM Mobile- 6.53 Inch FHD , 64GB, 4GB RAM, 4G LTE - Carbon Gray",
         rating: 2,
-        price: "7,990 EGP",
+        price: 7990
     },
     {
         id: "4",
@@ -40,7 +41,7 @@ var phoneProducts = [
         title: "iPhone 13 Pro",
         description: "iPhone 13 Pro (128 GB) - Yellow, Bluetooth,  Wi-Fi, USB",
         rating: 4,
-        price: "45,590 EGP",
+        price: 45590
     },
     {
         id: "5",
@@ -48,7 +49,7 @@ var phoneProducts = [
         title: "iPhone 12 Pro",
         description: "iPhone 12 Pro 128GB Graphite, Bluetooth,  Wi-Fi, USB",
         rating: 4,
-        price: "43,900 EGP",
+        price: 43900
     }
 ];
 
@@ -59,7 +60,7 @@ var computerProducts = [
         title: "ASUS Zenbook 14 Flip",
         description: "Laptop - AMD Ryzen 7 5800H, 16GB RAM, 1T SSD -Win 11 Home-Jade Black",
         rating: 4,
-        price: "35,660 EGP",
+        price: 35660
     },
     {
         id: "2",
@@ -67,7 +68,7 @@ var computerProducts = [
         title: "Lenovo IdeaPad Gaming 3",
         description: "Laptop - Ryzen 5 5600H, 16GB RAM, 1TB HDD + 256GB SSD, NVIDIA GeForce RTX 3050 Ti 4GB GDDR6 , 15.6' FHD 120Hz, Backlit KB, Windows11 - Black",
         rating: 3,
-        price: "30,750 EGP",
+        price: 30750
     },
     {
         id: "3",
@@ -75,7 +76,7 @@ var computerProducts = [
         title: "HP Victus 16-d1011ne Gaming",
         description: "Laptop, 12th Intel Core i7-12700H, 16GB RAM DDR5, 1TB NVMe SSD, NVIDIA RTX3060 6GB GDDR6, 16,1' FHD 144Hz, Backlit Keyboard, Dos, Blue",
         rating: 5,
-        price: "47,438 EGP",
+        price: 47438
     },
     {
         id: "4",
@@ -83,7 +84,7 @@ var computerProducts = [
         title: "Lenovo Legion 5 15ACH6",
         description: "Laptop - Ryzen 5 5600H , 8 GB RAM, 512 GB SSD, NVIDIA GeForce RTX 3050 Ti 4GB GDDR6 , 15.6' FHD 120Hz, Backlit Keyboard, Windows 11",
         rating: 3,
-        price: "33,120 EGP",
+        price: 33120
     },
     {
         id: "5",
@@ -91,7 +92,7 @@ var computerProducts = [
         title: "ASUS ROG Strix Scar 15",
         description: "Laptop G533QS-HF017T R9-5900HX 32GB 1TB SSD RTX3080 8GB 15.6' FHD 300Hz Win11 2Y Perfect Warranty",
         rating: 5,
-        price: "88,280 EGP",
+        price: 88280
     }
 ];
 
@@ -102,7 +103,7 @@ var smartwatchProducts = [
         title: "Apple Watch SE",
         description: "Apple Watch SE GPS 44MM Starlight Aluminum Case with Starlight Sport Band (2nd Gen - 2022)",
         rating: 5,
-        price: "14,999 EGP",
+        price: 14999
     },
     {
         id: "2",
@@ -110,7 +111,7 @@ var smartwatchProducts = [
         title: "Samsung SM-R870NZGAMEA",
         description: "Samsung SM-R870NZGAMEA Galaxy Watch4, 44mm, Green, 1 Year Warranty",
         rating: 4,
-        price: "7,012 EGP",
+        price: 7012
     },
     {
         id: "3",
@@ -118,7 +119,7 @@ var smartwatchProducts = [
         title: "HUAWEI WATCH GT 3",
         description: "HUAWEI WATCH GT 3 SE Smartwatch, Sleek and Stylish, Science-based Workouts, Sleep Health Monitoring.",
         rating: 3,
-        price: "6,459 EGP",
+        price: 6459
     },
     {
         id: "4",
@@ -126,7 +127,7 @@ var smartwatchProducts = [
         title: "Amazfit GTR 3 Pro",
         description: "Amazfit GTR 3 Pro Smart Watch for Android iPhone with Bluetooth Call Alexa GPS WiFi, Men's Fitness Tracker 150 Sports Modes, 1.45”AMOLED Display, Brown",
         rating: 4,
-        price: "7,759 EGP",
+        price: 7759
     },
     {
         id: "5",
@@ -134,7 +135,7 @@ var smartwatchProducts = [
         title: "Ultra Smartwatch 8",
         description: "Delone Smart Watch Ultra Smartwatch 8 Series 2” HD Screen Customize Dial Large Capacity Battery IP68 Waterproof (Orange)",
         rating: 2,
-        price: "1,200 EGP",
+        price: 1200
     }
 ];
 
@@ -145,7 +146,7 @@ var cameraProducts = [
         title: "INSTAX Mini 11 Instant",
         description: "INSTAX Mini 11 Instant Film Camera - Charcoal Gray Country of origin China Brand",
         rating: 2,
-        price: "2,999. EGP",
+        price: 2999
     },
     {
         id: "2",
@@ -153,7 +154,7 @@ var cameraProducts = [
         title: "Fujifilm Instax mini 9",
         description: "Fujifilm Instax mini 9 Instant Film Camera, Flamingo Pink",
         rating: 3,
-        price: "5,499 EGP",
+        price: 5499
     },
     {
         id: "3",
@@ -161,7 +162,7 @@ var cameraProducts = [
         title: "CANON EOS R10",
         description: "CANON EOS R10 + RF-S 18-150mm F3.5-6.3 IS STM",
         rating: 5,
-        price: "56,269 EGP",
+        price: 56269
     },
     {
         id: "4",
@@ -169,7 +170,7 @@ var cameraProducts = [
         title: "GoPro HERO11",
         description: "GoPro HERO11 Black - Waterproof Action Camera with 5.3K60 Ultra HD Video, 27MP Photos, 1/1.9' Image Sensor, Live Streaming, Webcam, Stabilization",
         rating: 3,
-        price: "18,216 EGP",
+        price: 18216
     },
     {
         id: "5",
@@ -177,7 +178,7 @@ var cameraProducts = [
         title: "Fujifilm instax mini liplay",
         description: "Fujifilm instax mini liplay hybrid digital camera - blush gold",
         rating: 1,
-        price: "6,049 EGP",
+        price: 6049
     }
 ];
 
@@ -188,7 +189,7 @@ var earphoneProducts = [
         title: "Xiaomi Buds 3 Active",
         description: "Xiaomi Buds 3 Active Noise Cancelling modes Dual device connectivity Supports Wireless charging 32h Long battery life with Charging Case Carbon Black, 480",
         rating: 2,
-        price: "2,450 EGP",
+        price: 2450
     },
     {
         id: "2",
@@ -196,7 +197,7 @@ var earphoneProducts = [
         title: "New bose quietcomfort 45",
         description: "New bose quietcomfort 45 bluetooth wireless noise canceling headphones - triple black",
         rating: 4,
-        price: "9,999 EGP",
+        price: 9999
     },
     {
         id: "3",
@@ -204,7 +205,7 @@ var earphoneProducts = [
         title: "HUAWEI WATCH GT 3 ",
         description: "HUAWEI WATCH GT 3 SE Smartwatch, Sleek and Stylish, Science-based Workouts, Sleep Health Monitoring.",
         rating: 3,
-        price: "6,459 EGP",
+        price: 6459
     },
     {
         id: "4",
@@ -212,7 +213,7 @@ var earphoneProducts = [
         title: "Amazfit GTR 3 Pro",
         description: "Amazfit GTR 3 Pro Smart Watch for Android iPhone with Bluetooth Call Alexa GPS WiFi, Men's Fitness Tracker 150 Sports Modes, 1.45”AMOLED Display, Brown",
         rating: 5,
-        price: "7,759 EGP",
+        price: 7759
     },
     {
         id: "5",
@@ -220,30 +221,49 @@ var earphoneProducts = [
         title: "Ultra Smartwatch 8",
         description: "Delone Smart Watch Ultra Smartwatch 8 Series 2” HD Screen Customize Dial Large Capacity Battery IP68 Waterproof (Orange)",
         rating: 2,
-        price: "1,200 EGP",
+        price: 1200
     }
 ];
 //////////////////////////////////////////////////
 // CartProduct
-var cartProduct = [];
 var cartItems = document.getElementById("cartItems");
 function createCartItems() {
+    var totalPrice = 0;
     cartItems.innerHTML = "";
+
     for (var i = 0; i < cartProduct.length; i++) {
+
         var cartItem = document.createElement('div');
         cartItem.className = "cartItem";
 
+        var itemImage = document.createElement('img');
+        itemImage.style.width = "25px";
+        itemImage.style.height = "25px";
+        itemImage.src = cartProduct[i].Product.image;
+
         var itemName = document.createElement('span');
         itemName.className = "item";
-        itemName.innerHTML = cartProduct[i].title;
+        itemName.innerHTML = cartProduct[i].Product.title;
+
+        var quantity = document.createElement('input');
+        quantity.type = "number";
+        quantity.className = "inputRange";
+        quantity.min = "1";
+        quantity.max = "10";
+        quantity.name = i;
+        quantity.value = cartProduct[i].Quantity;
+
+        quantity.onchange = function (event) {
+            updateQuantity(event);
+        }
 
         var itemQuantity = document.createElement('span');
         itemQuantity.className = "item";
-        itemQuantity.innerHTML = "1";
+        itemQuantity.appendChild(quantity);
 
         var itemPrice = document.createElement('span');
         itemPrice.className = "item";
-        itemPrice.innerHTML = cartProduct[i].price;
+        itemPrice.innerHTML = formatPrice(cartProduct[i].Product.price * cartProduct[i].Quantity);
 
         var itemDelete = document.createElement('span');
         itemDelete.className = "item";
@@ -254,9 +274,29 @@ function createCartItems() {
             createCartItems();
         }
 
-        cartItem.append(itemName, itemQuantity, itemPrice, itemDelete);
+        cartItem.append(itemImage, itemName, itemQuantity, itemPrice, itemDelete);
         cartItems.appendChild(cartItem);
+
+        totalPrice += (cartProduct[i].Product.price * cartProduct[i].Quantity);
     }
+    if (cartProduct.length == 0) {
+        document.getElementById("totalPriceDiv").style.display = "none";
+    } else {
+        document.getElementById("totalPriceDiv").style.display = "flex";
+    }
+    document.getElementById("totalPrice").innerHTML = formatPrice(totalPrice);
+}
+function formatPrice(price) {
+    return price.toLocaleString('en-EG', {
+        style: 'currency',
+        currency: 'EGP',
+        minimumFractionDigits: 0,
+    });
+}
+function updateQuantity(event) {
+    var index = Number(event.target.name);
+    cartProduct[index].Quantity = Number(event.target.value);
+    createCartItems();
 }
 //////////////////////////////////////////////////
 // Cart Model
@@ -369,7 +409,7 @@ function addProducts(dataProducts) {
         ratingProdact.append(createRatingBar(dataProducts[index].rating));
 
         var prodectPrice = document.createElement("span");
-        prodectPrice.innerHTML = dataProducts[index].price;
+        prodectPrice.innerHTML = formatPrice(dataProducts[index].price);
         ratingProdact.append(prodectPrice);
 
         var prodertBtn = document.createElement("button");
@@ -384,9 +424,8 @@ function addProducts(dataProducts) {
     var addCartButtons = document.getElementsByClassName("addCartButton");
     function onClickBtnCart(event) {
         var product = dataProducts[Number(event.target.id)];
-        cartProduct.push(product);
+        addProductinCart(product);
         createCartItems();
-        console.log(cartProduct);
     }
 
     for (var i = 0; i < addCartButtons.length; i++) {
@@ -395,6 +434,26 @@ function addProducts(dataProducts) {
         }
     }
 }
+
+function addProductinCart(product) {
+    let cartItem = { "Product": product, "Quantity": 1 };
+    let indexItem = findProductinCart(product);
+    if (indexItem == -1) {
+        cartProduct.push(cartItem);
+    } else {
+        cartProduct[indexItem].Quantity = cartProduct[indexItem].Quantity + 1;
+    }
+}
+
+function findProductinCart(product) {
+    for (var i = 0; i < cartProduct.length; i++) {
+        if (cartProduct[i].Product == product) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 //////////////////////////////////////////////////
 // Calling Function
 // Creates a Dot Of Sliders
